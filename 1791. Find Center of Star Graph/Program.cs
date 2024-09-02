a1f9c78f52d30caa1cf36e0a22d113f834e68b71
Suxrob Shadmanov <suxrobvjl0@gmail.com>
2024-09-02 14:55:36 +0500

    some changes

---
+++ 1791. Find Center of Star Graph/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1791.Find_Center_of_Star_Graph
+{
+    public class Solution
+    {
+        public int FindCenter(int[][] edges)
+        {
+          
+            
+                // Get the nodes from the first edge
+                int node1 = edges[0][0];
+                int node2 = edges[0][1];
+
+                // Check if either of these nodes is the center by checking the second edge
+                if (node1 == edges[1][0] || node1 == edges[1][1])
+                {
+                    return node1;
+                }
+                return node2;
+            }
+    }
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] edges = new int[3][];
+            edges[0] = new int[] {1,2 };
+            edges[1] = new int[] { 2, 3 };
+            edges[2] = new int[] { 4, 2 };
+
+            Solution solution = new Solution(); 
+            solution.FindCenter(edges); 
+
+        }
+    }
+}
---
+++ 997. Find the Town Judge 2 part/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _997.Find_the_Town_Judge_2_part
+{
+    public class Program
+    {
+        public class Solution
+        {
+            public int FindJudge(int n, int[][] trust)
+            {
+                int[] trustCount = new int[n + 1];
+                HashSet<int> candidates = new HashSet<int>();
+
+                foreach (var relation in trust)
+                {
+                    int a = relation[0];
+                    int b = relation[1];
+
+                    trustCount[b]++;
+                    candidates.Add(a);
+                }
+
+                for (int i = 1; i <= n; i++)
+                {
+                    if (trustCount[i] == n - 1 && !candidates.Contains(i))
+                    {
+                        return i;
+                    }
+                }
+
+                return -1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[][] inhabitants = new int[4][];
+            {
+                inhabitants[0] = new int[] { 0, 1 };
+                inhabitants[1] = new int[] { 4, 1 };
+                inhabitants[2] = new int[] { 3, 1 };
+                inhabitants[3] = new int[] { 2, 1 };
+            }
+
+            Solution sl = new Solution();
+            sl.FindJudge(1, inhabitants);
+        }
+    }
+}
